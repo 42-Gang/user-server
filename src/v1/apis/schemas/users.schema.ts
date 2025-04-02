5528dc656d8a6523075e00b706728f927346b624
kungbi <kipper12335@gmail.com>
2025-04-02 19:30:01 +0900

    feat(users.schema.ts): 사용자 스키마 기본값 추가

--- src/v1/apis/schemas/users.schema.ts
+++ src/v1/apis/schemas/users.schema.ts
@@ -1,12 +1,12 @@
 import { z } from 'zod';
 
 export const userSchema = z.object({
-  id: z.preprocess((val) => Number(val), z.number()),
-  nickname: z.string().min(2).max(8),
-  email: z.string().email(),
-  password_hash: z.string().optional(),
-  avatar_url: z.string(),
-  two_factor_auth: z.boolean(),
+  id: z.preprocess((val) => Number(val), z.number()).default(150),
+  nickname: z.string().min(2).max(8).default('kungbi'),
+  email: z.string().email().default('kungbi@gmail.com'),
+  password_hash: z.string().optional().default('#@(ghdgr9Y*$W'),
+  avatar_url: z.string().default('https://example.com/avatar.png'),
+  two_factor_auth: z.boolean().default(false),
   created_at: z.date(),
   updated_at: z.date(),
 });
