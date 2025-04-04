8c498ed9cefe4759ba4aaad7d3a33e625dff766b
kungbi <kipper12335@gmail.com>
2025-04-04 17:31:56 +0900

    feat: 소켓 서버 Redis 어댑터 추가

--- src/plugins/socket.ts
+++ src/plugins/socket.ts
@@ -1,6 +1,8 @@
 import { Server } from 'socket.io';
 import { FastifyInstance } from 'fastify';
 import { registerSocketGateway } from '../v1/sockets/gateway.js';
+import { createAdapter } from '@socket.io/redis-adapter';
+import { Redis } from 'ioredis';
 
 export function createSocketServer(fastify: FastifyInstance) {
   const socket = new Server(fastify.server, {
@@ -9,6 +11,14 @@ export function createSocketServer(fastify: FastifyInstance) {
     },
   });
   socket.logger = fastify.log;
+
+  const pubClient = new Redis({
+    host: process.env.REDIS_HOST || 'localhost',
+    port: Number(process.env.REDIS_PORT) || 6379,
+  });
+  const subClient = pubClient.duplicate();
+
+  socket.adapter(createAdapter(pubClient, subClient));
 
   registerSocketGateway(socket);
   return socket;
