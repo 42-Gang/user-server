58e7838ca6cda2e7ae0f01d9ab6b30d23701bbbb
kungbi <kipper12335@gmail.com>
2025-04-06 02:13:38 +0900

    feat: 친구 목록 API 연동 및 스키마 수정

--- src/v1/sockets/status/friends.schema.ts
+++ src/v1/sockets/status/friends.schema.ts
@@ -2,6 +2,8 @@ import { z } from 'zod';
 
 export const friendSchema = z.object({
   id: z.number(),
+  friend_id: z.number(),
+  nickname: z.string(),
 });
 
 export const friendsSchema = z.array(friendSchema);
--- src/v1/sockets/status/status.service.ts
+++ src/v1/sockets/status/status.service.ts
@@ -1,6 +1,7 @@
 import { FriendCacheInterface } from '../../storage/cache/interfaces/friend.cache.interface.js';
 import { friendsSchema } from './friends.schema.js';
 import { z } from 'zod';
+import { gotClient } from '../../../plugins/http.client.js';
 
 export default class StatusService {
   constructor(private readonly friendCacheRepository: FriendCacheInterface) {}
@@ -12,37 +13,18 @@ export default class StatusService {
       return cachedFriends;
     }
 
-    // const response = await gotClient.request<{
-    //   data: { friends: { id: number }[] };
-    // }>({
-    //   method: 'GET',
-    //   url: 'http://friends-server',
-    //   headers: {
-    //     'X-Authenticated': 'true',
-    //     'X-User-Id': userId.toString(),
-    //   },
-    // });
-    let response;
-    console.log('userId', typeof userId);
-    if (userId == 1) {
-      response = {
-        body: {
-          data: {
-            friends: [{ id: 2 }, { id: 3 }],
-          },
-        },
-      };
-    }
-    if (userId == 2) {
-      response = {
-        body: {
-          data: {
-            friends: [{ id: 1 }, { id: 3 }],
-          },
-        },
-      };
-    }
-    console.log('response', response);
+    const response = await gotClient.request<{
+      data: { friends: { id: number; friend_id: number; nickname: string }[] };
+    }>({
+      method: 'GET',
+      url: `http://localhost:8080/api/friends/v1/${userId}`,
+      headers: {
+        'X-Authenticated': 'true',
+        'X-User-Id': userId.toString(),
+      },
+    });
+
+    console.log('response', response.body);
     if (!response) {
       throw new Error('No friends found');
     }
