16f7a8ac4ea851bf808be80af70a9ec4eaff8db0
kungbi <kipper12335@gmail.com>
2025-04-06 01:28:40 +0900

    feat: 소켓 인증 미들웨어 추가 및 설정 변경

--- src/v1/sockets/utils/middleware.ts
+++ src/v1/sockets/utils/middleware.ts
@@ -1,34 +1,43 @@
-import { UnAuthorizedException } from '../../common/exceptions/core.error.js';
-import process from 'node:process';
+import { BadRequestException, UnAuthorizedException } from '../../common/exceptions/core.error.js';
 import { gotClient } from '../../../plugins/http.client.js';
 import { Socket } from 'socket.io';
+import { decode } from 'js-base64';
 
 type NextFunction = (err?: Error) => void;
 
 export async function socketMiddleware(socket: Socket, next: NextFunction) {
   try {
-    if (process.env.NODE_ENV === 'dev') {
-      socket.data.userId = socket.handshake.query.userId;
-      next();
-      return;
-    }
-
     const token = socket.handshake.query.token;
+    if (Array.isArray(token)) {
+      throw new BadRequestException('token은 배열이 될 수 없습니다.');
+    }
+    if (token === '') {
+      throw new BadRequestException('token은 빈 문자열이 될 수 없습니다.');
+    }
     if (!token) {
-      throw new UnAuthorizedException('인증되지 않은 사용자입니다.');
+      throw new BadRequestException('token이 없습니다.');
     }
 
-    const response = await gotClient.request<{ user_id: number }>({
+    const response = await gotClient.request({
       method: 'POST',
-      url: 'http://auth-server',
+      url: 'http://localhost:8080/auth/v1/token/verify',
       body: {
         access_token: token,
       },
     });
+    if (response.statusCode !== 200) {
+      throw new UnAuthorizedException('인증되지 않은 사용자입니다.');
+    }
 
-    socket.data.userId = Number(response.body.user_id);
+    const payload = token.split('.')[1];
+    console.log('payload', payload);
+    const decodedPayload = decode(payload);
+    const payloadObject: { id: number } = JSON.parse(decodedPayload);
+
+    socket.data.userId = Number(payloadObject.id);
     next();
   } catch (e) {
-    next(new UnAuthorizedException('인증되지 않은 사용자입니다.'));
+    console.error('Socket middleware error:', e);
+    next(e as Error);
   }
 }
