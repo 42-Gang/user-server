2f1a4edc7420013693dda1954ef66be5c070e845
kungbi <kipper12335@gmail.com>
2025-04-18 15:46:22 +0900

    test: 친구 서비스 테스트 수정

--- test/v1/apis/friends/friends.service.spec.ts
+++ test/v1/apis/friends/friends.service.spec.ts
@@ -29,6 +29,7 @@ const mockFriendRepository: FriendRepositoryInterface = {
   findAllByUserIdAndStatus: vi.fn(),
   findAllByFriendIdAndStatus: vi.fn(),
   findByUserIdAndFriendId: vi.fn(),
+  findAllByUserId: vi.fn(),
 };
 
 describe('FriendsService', () => {
