ed21821814904409391e68bac6a786fd36ebf158
kungbi <kipper12335@gmail.com>
2025-04-21 21:10:37 +0900

    refactor: 환경 변수를 사용하여 인증 서버 URL 설정
    인증 서버 URL을 localhost에서 환경 변수로 변경했습니다.

--- src/v1/sockets/utils/auth.ts
+++ src/v1/sockets/utils/auth.ts
@@ -4,7 +4,7 @@ import { gotClient } from '../../../plugins/http.client.js';
 export async function verifyAccessToken(token: string): Promise<number> {
   const response = await gotClient.request({
     method: 'POST',
-    url: 'http://localhost:8080/api/v1/auth/token/verify',
+    url: `http://${process.env.AUTH_SERVER}/api/v1/auth/token/verify`,
     body: { access_token: token },
   });
 
