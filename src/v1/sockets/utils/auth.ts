34b65e05844e6f30e7b223c4e4c5ba5ed6482934
kungbi <kipper12335@gmail.com>
2025-04-21 21:26:57 +0900

    refactor(auth): 토큰 검증 API 엔드포인트 변경 & header 변경

--- src/v1/sockets/utils/auth.ts
+++ src/v1/sockets/utils/auth.ts
@@ -4,8 +4,10 @@ import { gotClient } from '../../../plugins/http.client.js';
 export async function verifyAccessToken(token: string): Promise<number> {
   const response = await gotClient.request({
     method: 'POST',
-    url: `http://${process.env.AUTH_SERVER}/api/v1/auth/token/verify`,
-    body: { access_token: token },
+    url: `http://${process.env.AUTH_SERVER}/api/v1/auth/validate-token`,
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
   });
 
   return response.statusCode;
