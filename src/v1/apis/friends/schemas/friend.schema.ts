0d7740425f8a2d946c8780f7a03c17839a3b1620
kungbi <kipper12335@gmail.com>
2025-04-24 16:24:46 +0900

    refactor(friends): 스키마 필드명 카멜 케이스로 변경

--- src/v1/apis/friends/schemas/friend.schema.ts
+++ src/v1/apis/friends/schemas/friend.schema.ts
@@ -2,8 +2,8 @@ import { z } from 'zod';
 import { Status } from '@prisma/client';
 
 export const friendSchema = z.object({
-  friend_id: z.number().int(),
+  friendId: z.number().int(),
   nickname: z.string().min(2).max(8),
-  avatar_url: z.string().url(),
+  avatarUrl: z.string().url(),
   status: z.nativeEnum(Status),
 });
