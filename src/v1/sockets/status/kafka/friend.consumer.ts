7e7ac7e9c4f856344380fa15084bf663f816e9a5
Woongbi Shin <kipper12335@gmail.com>
2025-04-24 13:09:03 +0900

    refactor: 소켓 상태 Kafka Consumer 코드 정리 (#33)

--- src/v1/sockets/status/kafka/friend.consumer.ts
+++ src/v1/sockets/status/kafka/friend.consumer.ts
@@ -2,18 +2,12 @@ import { Namespace } from 'socket.io';
 import { redis } from '../../../../plugins/redis.js';
 import { TypeOf } from 'zod';
 import { friendAddMessage, friendBlockMessage } from './messages.schema.js';
-import { FriendCacheInterface } from '../../../storage/cache/interfaces/friend.cache.interface.js';
 
 export default class FriendConsumer {
-  constructor(
-    private readonly namespace: Namespace,
-    private readonly friendCacheRepository: FriendCacheInterface,
-  ) {}
+  constructor(private readonly namespace: Namespace) {}
 
   async handleFriendAddMessage(message: TypeOf<typeof friendAddMessage>) {
     const { userAId, userBId } = message;
-    // await this.friendCacheRepository.addFriend(userAId, { friendId: userBId });
-    // await this.friendCacheRepository.addFriend(userBId, { friendId: userAId });
 
     console.log(message);
     await this.joinFriendStatusRooms(this.namespace, userAId.toString(), userBId.toString());
@@ -23,22 +17,12 @@ export default class FriendConsumer {
   async handleFriendBlockMessage(message: TypeOf<typeof friendBlockMessage>) {
     const { fromUserId, toUserId } = message;
 
-    // await redis.del(`user:${fromUserId}:friend:${toUserId}`);
-    // await redis.del(`user:${toUserId}:friend:${fromUserId}`);
-
     const toUserSocket = this.namespace.in(`user:${toUserId}`);
     toUserSocket?.socketsLeave(`user-status-${fromUserId}`);
   }
 
   async handleFriendUnblockMessage(message: TypeOf<typeof friendBlockMessage>) {
     const { fromUserId, toUserId } = message;
-
-    // await this.friendCacheRepository.addFriend(Number(fromUserId), {
-    //   friendId: Number(toUserId),
-    // });
-    // await this.friendCacheRepository.addFriend(Number(toUserId), {
-    //   friendId: Number(fromUserId),
-    // });
 
     const toUserSocket = this.namespace.in(`user:${toUserId}`);
     toUserSocket?.socketsJoin(`user-status-${fromUserId}`);
