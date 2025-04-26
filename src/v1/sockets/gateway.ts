0ff94caa417193e2352fd8eb24a4d6c58cb810d2
kungbi <kipper12335@gmail.com>
2025-04-26 17:58:05 +0900

    feat: 소켓 게이트웨이 - 친구 네임스페이스 추가

--- src/v1/sockets/gateway.ts
+++ src/v1/sockets/gateway.ts
@@ -2,15 +2,19 @@ import { Server } from 'socket.io';
 import startStatusNamespace from './status/start-status-namespace.js';
 import { socketMiddleware } from './utils/middleware.js';
 import { asValue, AwilixContainer } from 'awilix';
+import startFriendNamespace from './friend/start-friend-namespace.js';
 
 export const registerSocketGateway = (diContainer: AwilixContainer, io: Server) => {
   io.use(socketMiddleware);
 
   const statusNamespace = io.of('/status');
+  const friendNamespace = io.of('/friend');
 
   diContainer.register({
     statusNamespace: asValue(statusNamespace),
+    friendNamespace: asValue(friendNamespace),
   });
 
   startStatusNamespace(statusNamespace);
+  startFriendNamespace(friendNamespace);
 };
