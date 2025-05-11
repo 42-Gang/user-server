8fc6b04d0591f9ac43f7af6f73335c2a85025ef0
cherrynuna <inyoungstar7@gmail.com>
2025-05-11 16:45:45 +0900

    Fix: avatarURL이 상대경로로 변경됨에 따른 수정

--- src/v1/apis/friends/schemas/friend.schema.ts
+++ src/v1/apis/friends/schemas/friend.schema.ts
@@ -3,7 +3,7 @@ import { Status } from '@prisma/client';
 
 export const friendSchema = z.object({
   friendId: z.number().int(),
-  nickname: z.string().min(2).max(8),
-  avatarUrl: z.string().url(),
+  nickname: z.string(),
+  avatarUrl: z.string(),
   status: z.nativeEnum(Status),
 });
