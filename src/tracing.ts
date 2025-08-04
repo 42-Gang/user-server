e816e3b27d6f848eb9fc8e3e5b521f02cd25dd42
kungbi <kipper12335@gmail.com>
2025-08-04 11:51:03 +0900

    feat(tracing.ts): Prisma Instrumentation 추가

--- src/tracing.ts
+++ src/tracing.ts
@@ -1,18 +1,7 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
-
-const prometheusExporter = new PrometheusExporter(
-  {
-    host: '0.0.0.0',
-    port: 9464,
-    endpoint: '/metrics',
-  },
-  () => {
-    console.log('✅ Prometheus scrape endpoint: http://0.0.0.0:9464/metrics');
-  },
-);
+import { PrismaInstrumentation } from '@prisma/instrumentation';
 
 if (!process.env.JAEGER_ENDPOINT) {
   throw new Error('JAEGER_ENDPOINT environment variable is not set');
@@ -25,8 +14,7 @@ const sdk = new NodeSDK({
     headers: {},
   }),
   serviceName: 'user-service',
-  metricReader: prometheusExporter,
-  instrumentations: [getNodeAutoInstrumentations()],
+  instrumentations: [getNodeAutoInstrumentations(), new PrismaInstrumentation()],
 });
 
 sdk.start();
