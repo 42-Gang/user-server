42c3bf65babd65406799780d25db8cc002b46616
kungbi <kipper12335@gmail.com>
2025-08-30 22:07:35 +0900

    feat(tracing.ts): refactor Kafka message hooks for attribute setting

--- src/tracing.ts
+++ src/tracing.ts
@@ -2,11 +2,30 @@ import { NodeSDK } from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { PrismaInstrumentation } from '@prisma/instrumentation';
+import { Span } from '@opentelemetry/api';
 
 if (!process.env.JAEGER_ENDPOINT) {
   throw new Error('JAEGER_ENDPOINT environment variable is not set');
 }
 console.log(`Using Jaeger endpoint: ${process.env.JAEGER_ENDPOINT}`);
+
+interface KafkajsMessage {
+  key?: Buffer | string | null;
+  value: Buffer | string | null;
+  partition?: number;
+  headers?: Record<string, Buffer | string | (Buffer | string)[] | undefined>;
+  timestamp?: string;
+}
+
+const kafkaMessageHook = (
+  span: Span,
+  { topic, message }: { topic: string; message: KafkajsMessage },
+) =>
+  span.setAttributes({
+    'kafka.topic': topic,
+    'kafka.message.key': message.key ? message.key.toString() : undefined,
+    'kafka.message.value.length': message.value ? message.value.length : undefined,
+  });
 
 const sdk = new NodeSDK({
   traceExporter: new OTLPTraceExporter({
@@ -18,18 +37,8 @@ const sdk = new NodeSDK({
     getNodeAutoInstrumentations({
       '@opentelemetry/instrumentation-kafkajs': {
         enabled: true,
-        producerHook: (span, { topic, message }) =>
-          span.setAttributes({
-            'kafka.topic': topic,
-            'kafka.message.key': message.key ? message.key.toString() : undefined,
-            'kafka.message.value': message.value ? message.value.toString() : undefined,
-          }),
-        consumerHook: (span, { topic, message }) =>
-          span.setAttributes({
-            'kafka.topic': topic,
-            'kafka.message.key': message.key ? message.key.toString() : undefined,
-            'kafka.message.value': message.value ? message.value.toString() : undefined,
-          }),
+        producerHook: kafkaMessageHook,
+        consumerHook: kafkaMessageHook,
       },
     }),
     new PrismaInstrumentation(),
