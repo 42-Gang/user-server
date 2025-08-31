b26e3dca5bda9153a3973a584397be908d9392bf
kungbi <kipper12335@gmail.com>
2025-08-31 19:49:24 +0900

    feat(tracing.ts): update Kafka message hook to use kafkajs Message type

--- src/tracing.ts
+++ src/tracing.ts
@@ -3,24 +3,14 @@ import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentation
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { PrismaInstrumentation } from '@prisma/instrumentation';
 import { Span } from '@opentelemetry/api';
+import { Message } from 'kafkajs';
 
 if (!process.env.JAEGER_ENDPOINT) {
   throw new Error('JAEGER_ENDPOINT environment variable is not set');
 }
 console.log(`Using Jaeger endpoint: ${process.env.JAEGER_ENDPOINT}`);
 
-interface KafkajsMessage {
-  key?: Buffer | string | null;
-  value: Buffer | string | null;
-  partition?: number;
-  headers?: Record<string, Buffer | string | (Buffer | string)[] | undefined>;
-  timestamp?: string;
-}
-
-const kafkaMessageHook = (
-  span: Span,
-  { topic, message }: { topic: string; message: KafkajsMessage },
-) =>
+const kafkaMessageHook = (span: Span, { topic, message }: { topic: string; message: Message }) =>
   span.setAttributes({
     'kafka.topic': topic,
     'kafka.message.key': message.key ? message.key.toString() : undefined,
